79e45b4568cb2bae16c0840a89270a70f6121f39
Nikita <nikita@TM-ND0028.local>
2020-09-15 12:40:06 -0700

    supported upload of file

--- index.js
+++ index.js
@@ -14,7 +14,19 @@ const buttonMapping = {
         buttonColor: "#f2d14b",
         image: "static/yellow-umbrella.png",
     }
-}
+};
+
+window.addEventListener('load', function() {
+    document.querySelector('input[type="file"]').addEventListener('change', function() {
+        if (this.files && this.files[0]) {
+            var img = document.querySelector('#logo-image');  // $('img')[0]
+            img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+            img.style.display = 'inline-block';
+            document.getElementById("upload-label").innerHTML = this.files[0].name;
+        }
+    });
+});
+
 function colorSwitch(color) {
     document.getElementById("umbrella-image").src = buttonMapping[color].image;
     document.getElementById("upload-button").style.backgroundColor = buttonMapping[color].buttonColor;
