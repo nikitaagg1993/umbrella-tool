a41baf93b79d936b2d3b6e6c27898126d288e4f7
Nikita <nikita@TM-ND0028.local>
2020-09-16 00:02:45 -0700

    Added functionality to apply logo in umbrella

--- index.js
+++ index.js
@@ -16,19 +16,50 @@ const buttonMapping = {
     }
 };
 
+let uploadedFile;
+
+function addLogo(umbrella) {
+    if (uploadedFile && uploadedFile[0]) {
+        let loader = document.getElementById("umbrella-image")
+        let imageSource = loader.src;
+        // loader.style.display = 'none';
+
+        let svg = document.getElementById("loader-svg");
+        svg.style.fill = "pink";
+        loader.src="static/loader_icon.svg";
+        loader.className="loader";
+
+        let uploadButton = document.getElementById("upload-icon");
+        uploadButton.src="static/loader_icon.svg"
+        // uploadButton.style.height = "25px";
+        uploadButton.className="uploadIcon loaderIcon";
+
+        let img = document.querySelector('#logo-image');
+        img.style.display = 'none';
+        document.getElementById("label-text").innerHTML = uploadedFile[0].name;
+
+
+        setTimeout(() => {
+            uploadButton.src = "static/upload_icon.svg";
+            uploadButton.className = "uploadIcon";
+            loader.src = umbrella || imageSource;
+            loader.className = "umbrellaImage";
+              // $('img')[0]
+            img.src = URL.createObjectURL(uploadedFile[0]); // set src to blob url
+            img.style.display = 'inline-block';
+        }, 100000)
+    }
+}
 window.addEventListener('load', function() {
     document.querySelector('input[type="file"]').addEventListener('change', function() {
-        if (this.files && this.files[0]) {
-            var img = document.querySelector('#logo-image');  // $('img')[0]
-            img.src = URL.createObjectURL(this.files[0]); // set src to blob url
-            img.style.display = 'inline-block';
-            document.getElementById("upload-label").innerHTML = this.files[0].name;
-        }
+        uploadedFile = this.files;
+        addLogo();
     });
 });
 
 function colorSwitch(color) {
-    document.getElementById("umbrella-image").src = buttonMapping[color].image;
-    document.getElementById("upload-button").style.backgroundColor = buttonMapping[color].buttonColor;
+    addLogo(buttonMapping[color].image);
+    // document.getElementById("umbrella-image").src = buttonMapping[color].image;
+    document.getElementById("upload-label").style.backgroundColor = buttonMapping[color].buttonColor;
     document.body.style.backgroundColor = buttonMapping[color].backgroundColor;
 }
